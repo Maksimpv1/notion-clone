daf84285e702b6b266ab1076a4e91e676acd1fc7
Maksim <maksimpv1@bk.ru>
2025-01-16 15:51:47 +0300

    fix

--- notion-front/firebase-admin.ts
+++ notion-front/firebase-admin.ts
@@ -7,8 +7,10 @@ let app: App;
 const serviceKey:ServiceAccount = {
   projectId: "notion-clone-87a7a",
   clientEmail: "firebase-adminsdk-1h5yv@notion-clone-87a7a.iam.gserviceaccount.com",
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\n/g, '\n')!,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')!,
 };
+
+const serviceKeyOne = require('./service_key.json'); 
 
 if (getApps().length === 0) {
   app = initializeApp({
